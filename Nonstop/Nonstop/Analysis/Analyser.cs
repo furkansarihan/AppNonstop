c54c89d96db0366952b84f9ef23a03a594295120
Furkan <furkan.sarihan@stu.fsm.edu.tr>
2018-11-01 21:08:50 +0300

    initilaize for Analysis and Game
    Analysis class, its base functionality and its main relation with Game class described.

---
+++ Nonstop/Nonstop/Analysis/Analyser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nonstop.Forms.Analysis
+{
+    /*
+     * This class generates and manages runtime data for Nonstop 
+     * 
+     **/
+    class Analyser
+    {
+        /*
+         * This function analyses and generates runtime data for game,
+         * and writes this data to spesific path for next uses.
+         * *Xform*: The name of runtime data format that readed in game
+         * 
+         * Input: track id for spesific Spotify Track
+         * Output: bool value for result of operations.
+         * 
+         **/
+        public bool generateXform(String trackId)
+        {
+            // Getting analyzer data from api.spotify request
+            // Creating runtime data from api.spotify response json............
+            // Writing data to path -> trackId.json
+            return true;
+        }
+
+        /*
+         * This function initilases a Xform object for given track.
+         * Returnes false if trackId.json not exsist in path
+         * 
+         * Input1: Xform object pointer for caller *game* class, 
+         * Input2: track id for spesific Spotify Track
+         * Output: bool value for result of operations.
+         **/
+        public bool setupXform(Xform runtimeData, String trackId)
+        {
+            // Control path for existing generated runtime data
+            // return false if do not exist
+            
+            runtimeData = new Xform(trackId);
+            return true;
+        }
+    }
+}
---
+++ Nonstop/Nonstop/Analysis/Xform.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nonstop.Forms.Analysis
+{
+    /*
+     * This class includes playable runtime data and It's specified format 
+     * 
+     **/
+    class Xform
+    {
+        public Xform(String trackId)
+        {
+            // Reads trackId.json from path and fills itself
+        }
+    }
+}
