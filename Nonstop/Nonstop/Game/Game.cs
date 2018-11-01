7f925d4b9e8a2c51f646232f77719a4e9c5d3753
Furkan <furkan.sarihan@stu.fsm.edu.tr>
2018-11-01 23:08:25 +0300

    UrhoSharp initialisation
    UrhoSurface works on Android

--- Nonstop/Nonstop/Game/Game.cs
+++ Nonstop/Nonstop/Game/Game.cs
@@ -2,36 +2,68 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Urho;
+using Urho.Actions;
+using Urho.Gui;
+using Urho.Shapes;
+
 using Nonstop.Forms.Analysis;
 
 namespace Nonstop.Forms.Game
 {
-    class Game
+    /*
+     * This class includes all Urho3D code, NOT anything beyond that.
+     * Everything about controlling syncronisation management will make on parent class
+     * 
+     **/
+    class Game : Urho.Application
     {
-        Xform runtimeData;
-        Analyser analyser;
-        // SpotifyRemoteApp remoteCommunicator;
+        Scene scene;
+        Node plotNode;
+        Camera camera;
+        Octree octree;
 
-        public Game(String trackId)
+        Xform runtimeData;
+
+        public Game(ApplicationOptions options = null) : base(options) { }
+
+        protected override void Start()
         {
-            analyser = new Analyser(); // Generating analyser object
-            if (!analyser.setupXform(runtimeData, trackId)) // Check if runtime data exist for spesified track
-            {
-                // Runtime data is not exist, sooo generate it..
-                if (analyser.generateXform(trackId)) // Check for errors
-                {
-                    // Runtime data Succesfully generated and writed to file, so
-                    analyser.setupXform(runtimeData, trackId); // runtimeData gets valid runtime data
-                    // Create and warmup game assets..
-                    // Make request to remoteApp object to play specified track
-                    // syncronise the game time with time getting from spotify playerState
-                    // then start GAMELOOP :)
-                }
-                else
-                {
-                    // Error happened when generating runtime data.
-                }
-            } 
+            base.Start();
+            createScene();
         }
+        protected override void OnUpdate(float timeStep)
+        {
+            base.OnUpdate(timeStep);
+        }
+        public void createScene()
+        {
+            
+            scene = new Scene();
+            octree = scene.CreateComponent<Octree>();
+
+            plotNode = scene.CreateChild();
+            var baseNode = plotNode.CreateChild().CreateChild();
+
+            var plane = baseNode.CreateComponent<StaticModel>();
+            plane.Model = CoreAssets.Models.Plane;
+            
+            var cameraNode = scene.CreateChild();
+            camera = cameraNode.CreateComponent<Camera>();
+            cameraNode.Position = new Vector3(10, 15, 10) / 1.75f;
+            cameraNode.Rotation = new Quaternion(-0.121f, 0.878f, -0.305f, -0.35f);
+
+            Node lightNode = cameraNode.CreateChild();
+            var light = lightNode.CreateComponent<Light>();
+            light.LightType = LightType.Point;
+            light.Range = 100;
+            light.Brightness = 1.3f;
+            
+        }
+        public void setXform(Xform data)
+        {
+            this.runtimeData = data;
+        }
+        
     }
 }
--- Nonstop/Nonstop/MainPage.xaml.cs
+++ Nonstop/Nonstop/MainPage.xaml.cs
@@ -8,25 +8,50 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using Urho;
+using Urho.Forms;
+
+using Nonstop.Forms.Game;
+
 namespace Nonstop
 {
     public partial class MainPage : ContentPage
     {
+        UrhoSurface urhoSurface;
+        Game urhoGame;
+        GameManager gameManager;
+
         public MainPage()
         {
             InitializeComponent();
 
             var assembly = typeof(MainPage).GetTypeInfo().Assembly;
             Stream stream = assembly.GetManifestResourceStream("Nonstop.Forms.Spotify.track.json");
-            
+
             using (var reader = new System.IO.StreamReader(stream))
             {
                 var json = reader.ReadToEnd();
                 System.Console.WriteLine(json);
                 var data = JsonConvert.DeserializeObject<Track>(json);
             }
-            
-            
+
+            // Urho Surface
+            urhoSurface = new UrhoSurface();
+            urhoSurface.VerticalOptions = LayoutOptions.FillAndExpand;
+            Content = new StackLayout
+            {
+                Padding = new Thickness(1, 1, 1, 1),
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children = { urhoSurface }
+            };
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            urhoGame = await urhoSurface.Show<Game>(new ApplicationOptions(assetsFolder: null) { Orientation = ApplicationOptions.OrientationType.Portrait });
+            gameManager = new GameManager(urhoGame, "sampleTrackId");
         }
     }
 }
